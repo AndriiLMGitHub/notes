f0b40ec8e35ee24aa8bf54d20dd18907fc0816e9
Mac <mac@MacBook-Air.local>
2024-06-12 21:31:22 +0300

    static

--- server/core/settings.py
+++ server/core/settings.py
@@ -128,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_ROOT = BASE_DIR / 'client/build/static'
 STATICFILES_DIRS = (
     BASE_DIR / 'client/build/static',
 )
